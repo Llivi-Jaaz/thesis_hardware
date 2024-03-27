a8e07292cfd429b6ade00ef2e3ec55b1f003d8fe
Allivia Jaazielle Caina <138189751+Llivi-Jaaz@users.noreply.github.com>
2024-03-28 04:03:05 +0800

    Create TRANSMITTER.ino

---
+++ TRANSMITTER.ino
@@ -0,0 +1,49 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+#define ss D8
+#define rst D1
+#define dio0 D2
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("LoRa Receiver");
+
+  LoRa.setPins(ss, rst, dio0);
+  
+  //433E6 for Asia
+  if(!LoRa.begin(433E6)) {
+    Serial.println(".");
+    while (1);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+}
+
+void loop() 
+{
+
+  String LoRaData; 
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) 
+  {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available())
+    {
+      LoRaData = LoRa.readString();
+      Serial.print(LoRaData); 
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+    
+   }
+}
