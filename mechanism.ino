307120c64420f428165fde19ce6be83506b7f015
Allivia Jaazielle Caina <138189751+Llivi-Jaaz@users.noreply.github.com>
2024-04-25 17:23:24 +0800

    Update mechanism.ino

--- mechanism.ino
+++ mechanism.ino
@@ -1,28 +1,41 @@
-  const int fltswtch = 8;
-  const int relay_valve = 7;
-  const int relay_actuator = 6;
+const int fltswtch = 8;
+const int relay_valve = 7;
+const int relay_actuator = 6;
 
-  void setup()   
-  {   
-  pinMode(relay_valve,OUTPUT);
-   pinMode(relay_actuator,OUTPUT);
+bool isProcessRunning = false;
+unsigned long startTime;
+
+void setup() {
+  pinMode(relay_valve, OUTPUT);
+  pinMode(relay_actuator, OUTPUT);
   pinMode(fltswtch, INPUT_PULLUP);
-  }   
-  void loop()    
-  {   
+}
+
+void loop() {
   int floatState = digitalRead(fltswtch);
 
-  if (floatState == 1) {
-  delay(2000);
-  digitalWrite(relay_valve,HIGH);
-  digitalWrite(relay_actuator,HIGH);
-  delay(5000);
-  digitalWrite(relay_valve, LOW);
-  delay(25000);
-  digitalWrite(relay_actuator, LOW);
-  } 
-  else {
+  if (floatState == HIGH && !isProcessRunning) {
+    startProcess();
+  }
+
+  if (isProcessRunning) {
+    unsigned long currentTime = millis(); //returns the number of milliseconds since the Arduino board began running
+    if (currentTime - startTime >= 5000) {
+      digitalWrite(relay_valve, LOW);
+    }
+    if (currentTime - startTime >= 30000) {
+      digitalWrite(relay_actuator, LOW);
+      isProcessRunning = false;
+    }
+  } else {
     digitalWrite(relay_valve, LOW);
     digitalWrite(relay_actuator, LOW);
   }
-  } 
+}
+
+void startProcess() {
+  isProcessRunning = true;
+  startTime = millis();
+  digitalWrite(relay_valve, HIGH);
+  digitalWrite(relay_actuator, HIGH);
+}
